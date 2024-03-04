847a6a48ec62e82fa357f0a07c016c002e3b4050
Tae Hyeon Jo <hillskuti@naver.com>
2024-03-04 23:08:00 +0900

    NextJS 예약어 정리

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
-import './globals.css'
+// 모든 페이지에 'globals.css' 사용가능
+import './globals.css';
 
+// app 폴더에 'icon.png' 파일은 파비콘으로 동작한다.
 export const metadata = {
   title: 'NextJS Course App',
   description: 'Your first NextJS app!',
@@ -7,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );
