9f3a60a08ef18986406e17879503c120a3fb9448
Tae Hyeon Jo <hillskuti@naver.com>
2024-03-24 22:17:25 +0900

    NextJS14 다이나믹 routes 및 parameters 적용

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,38 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import Image from 'next/image';
+
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
+// 모든 page.js는 params 값을 자동으로 받게 된다.
+// [mealsSlug] 안에 들어간 'mealsSlug'가 key 값이 된다.
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}> {meal.creator} </a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        {/* dangerouslySetInnerHTML을 사용하면 크로스사이트 스크립팅 공격에 노출된다. */}
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
