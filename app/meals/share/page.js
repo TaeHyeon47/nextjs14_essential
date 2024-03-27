6774b7df7e9cd06842b8d3fb42afb10df9e8fb17
Tae Hyeon Jo <hillskuti@naver.com>
2024-03-27 23:10:21 +0900

    nextjs14 use client 환경에도 use server 가능하도록 변경

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,24 +1,10 @@
+'use client';
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-  // 'use server'는 서버 액션을 만드는데 서버에서 실행된다는 것이 보장된다.
-  // shareMeal는 서버에서만 사용된다.
-  async function shareMeal(formData) {
-    'use server';
-
-    const meal = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email'),
-    };
-
-    console.log(meal);
-  }
-
   return (
     <>
       <header className={classes.header}>
